5affb618b3cd5333d6b863d4e8723fd3fa217a66
triet3 <trietpt09@gmail.com>
2024-11-17 13:29:37 +0700

    triet update

--- src/main/java/com/donation/donation_system/controller/UserController.java
+++ src/main/java/com/donation/donation_system/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.donation.donation_system.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
-    @GetMapping("/")
-    public String homepage() {
-        return "index";  // Trả về trang index.html
-    }
+
+        @GetMapping("/password")
+        public String changePassword(@RequestParam(name = "action", required = false) String action) {
+            if ("changepassword".equals(action)) {
+                return "changepassword";
+            }
+            return "redirect:/";
+        }
 }
--- src/main/java/com/donation/donation_system/service/FundService.java
+++ src/main/java/com/donation/donation_system/service/FundService.java
@@ -1,4 +1,21 @@
 package com.donation.donation_system.service;
 
+import com.donation.donation_system.model.Fund;
+import com.donation.donation_system.repository.FundRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface FundService {
+    List<Fund> FindAll();
+    Optional<Fund> findById(int id);
+    Fund save(Fund fund);
+    List<Fund> saveAllAndFlush(Iterable<Fund> funds);
+    void flush();
+    void delete(Fund fund);
+    void deleteById(int id);
+    void deleteAll();
+    void deleteAllInBatch(Iterable<Fund> funds);
+    Fund saveAndFlush(Fund fund);
+
 }
