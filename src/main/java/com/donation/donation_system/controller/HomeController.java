d150585852c5f579038b9b2fc3f2880f297b126d
triet3 <trietpt09@gmail.com>
2024-11-18 19:28:45 +0700

    update đổ dữ liệu lên giao diện

--- src/main/java/com/donation/donation_system/controller/HomeController.java
+++ src/main/java/com/donation/donation_system/controller/HomeController.java
@@ -1,16 +1,29 @@
 package com.donation.donation_system.controller;
 
+import com.donation.donation_system.model.Fund;
 import com.donation.donation_system.model.User;
+import com.donation.donation_system.service.FundService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 
 @Controller
 @RequestMapping("/Donations")
 public class HomeController {
+
+    private final FundService fundService;
+
+    @Autowired
+    public HomeController(FundService fundService) {
+        this.fundService = fundService;
+    }
+
     @GetMapping("/admin/home")
     public String admin(Model model, HttpSession session) {
         User user = (User) session.getAttribute("user");
@@ -27,6 +40,10 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("content", "/pages/home"); // Nạp fragment home
+        List<Fund> funds = fundService.FindAll();
+
+        // Thêm danh sách quỹ vào model để truyền sang view
+        model.addAttribute("funds", funds);
         return "index";
     }
     @GetMapping("/detailFragment")
